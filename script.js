c8b72156a5dfcbf5b778700eeb1be4fcc0997c26
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-07 22:49:52 -0400

    Adjust style colors

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ const container = document.querySelector("#container")
 const content = document.createElement("div")
 
 content.classList.add("content")
-content.textContent("This was added via Javascript")
+content.textContent = "This was added via Javascript";
 
 container.appendChild(content)
 
 const redText = document.createElement("div")
-redText.setAttribute("redText")
-redText.setAttribute("style", "color: red;")
-redText.textContent("Hey I'm Red!")
+redText.classList.toggle("redText")
+redText.style.color("red")
+redText.textContent = "Hey I am Red!";
 
 const blueHeader3 = document.createElement("div")
 blueHeader3.classList.toggle("blueHeader3")
-blueHeader3.setAttribute("style", "color: blue;");
-blueHeader3.textContent("Hey I am Blue!")
+blueHeader3.style.color("blue")
+blueHeader3.textContent = "Hey I am Blue!"
 
 const blackBox = document.createElement("div")
 blackBox.classList.toggle("blackBox")
-blackBox.setAttribute(border: 4px solid black; backgroundColor: pink;)
+blackBox.setAttribute("border: 4px solid black; backgroundColor: pink;")
 
 const headerInsideBlackBox = document.createElement("h1")
 headerInsideBlackBox.classList.toggle("headerInsideBlackBox")
@@ -27,7 +27,7 @@ headerInsideBlackBox.textContent("This is the header inside the box")
 
 const textInsideBlackBox = document.createElement("p")
 textInsideBlackBox.classList.toggle("textInsideBlackBox")
-textInsideBlackBox.style.color("Black")
+textInsideBlackBox.style.color("black")
 
 blackBox.appendChild(headerInsideBlackBox)
 blackBox.appendChild(textInsideBlackBox)
