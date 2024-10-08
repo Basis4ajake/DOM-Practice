99cb3ea28ca4e9e634c0211591592c5b7791e4b1
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-07 22:58:26 -0400

    Add button through html onclick property

--- script.js
+++ script.js
@@ -8,26 +8,27 @@ container.appendChild(content)
 
 const redText = document.createElement("div")
 redText.classList.toggle("redText")
-redText.style.color("red")
+redText.style.color = "red";
 redText.textContent = "Hey I am Red!";
 
 const blueHeader3 = document.createElement("div")
 blueHeader3.classList.toggle("blueHeader3")
-blueHeader3.style.color("blue")
+blueHeader3.style.color = "blue";
 blueHeader3.textContent = "Hey I am Blue!"
 
 const blackBox = document.createElement("div")
 blackBox.classList.toggle("blackBox")
-blackBox.setAttribute("border: 4px solid black; backgroundColor: pink;")
+blackBox.setAttribute("style", "border: 4px solid black; backgroundColor: pink;")
 
 const headerInsideBlackBox = document.createElement("h1")
 headerInsideBlackBox.classList.toggle("headerInsideBlackBox")
-headerInsideBlackBox.style.color("black")
-headerInsideBlackBox.textContent("This is the header inside the box")
+headerInsideBlackBox.style.color = "black";
+headerInsideBlackBox.textContent = "This is the header inside the box";
 
 const textInsideBlackBox = document.createElement("p")
 textInsideBlackBox.classList.toggle("textInsideBlackBox")
-textInsideBlackBox.style.color("black")
+textInsideBlackBox.style.color = "black";
+textInsideBlackBox.textContent = "This is the new text in the box";
 
 blackBox.appendChild(headerInsideBlackBox)
 blackBox.appendChild(textInsideBlackBox)
