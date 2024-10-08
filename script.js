13110638f73534f6ea025e98d85ae4e8b3a5f2b3
Jacob Marlowe <jacob.marlowe1991@gmail.com>
2024-10-07 22:12:09 -0400

    Add Blue Header

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ const redText = document.createElement("div")
 redText.setAttribute("redText")
 redText.setAttribute("style", "color: red;")
 redText.textContent("Hey I'm Red!")
+
+const blueH3 = document.createElement("div")
+blueH3.classList.toggle("blueHeader")
+blueH3.setAttribute("style", "color: blue;");
+blueH3.textContent("Hey I am Blue!")
